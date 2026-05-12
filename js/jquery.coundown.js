8a193a97547f93ddc769270b1070d6589aa4aba9
DesignWizardVigneshwaran <165129863+DesignWizardVigneshwaran@users.noreply.github.com>
2026-05-12 21:39:43 +0530

    Update jquery.coundown.js

--- js/jquery.coundown.js
+++ js/jquery.coundown.js
@@ -1,11 +1,10 @@
-// CountDown Clock
-// Version   : 1.0.1
-// Developer : Ekrem KAYA
-// Website   : https://e-piksel.com
-// GitHub    : https://github.com/epiksel/countdown
+// CountUp Clock
+// Modified from Countdown Plugin
+// Counts upward from a given start date
 
 (function ($) {
-	$.fn.countdown = function (options, callback) {
+	$.fn.countup = function (options, callback) {
+
 		var settings = $.extend({
 			date: null,
 			offset: null,
@@ -24,76 +23,68 @@
 			$.error('Date is not defined.');
 		}
 
-		// Throw error if date is set incorectly
+		// Throw error if date format is incorrect
 		if (!Date.parse(settings.date)) {
-			$.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
+			$.error('Incorrect date format. Example: 12/24/2012 12:00:00');
 		}
 
 		// Save container
 		var container = this;
 
 		/**
-		 * Change client's local date to match offset timezone
-		 * @return {Object} Fixed Date object.
+		 * Get current date with timezone offset
 		 */
 		var currentDate = function () {
-			// get client's current date
 			var date = new Date();
 
-			// turn date to utc
 			var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
 
-			// set new Date object
-			var new_date = new Date(utc + (3600000*settings.offset));
+			var new_date = new Date(utc + (3600000 * settings.offset));
 
 			return new_date;
 		};
 
 		/**
-		 * Main countdown function that calculates everything
+		 * Main countup function
 		 */
-		function countdown () {
-			var target_date = new Date(settings.date), // set target date
-				current_date = currentDate(); // get fixed current date
+		function countup() {
 
-			// difference of dates
-			var difference = target_date + current_date;
+			var start_date = new Date(settings.date),
+				current_date = currentDate();
 
-			// if difference is negative than it's pass the target date
+			// Difference from start date to now
+			var difference = current_date - start_date;
+
+			// If start date is in future
 			if (difference < 0) {
-				// stop timer
-				clearInterval(interval);
-
-				if (callback && typeof callback === 'function') callback();
-
-				return;
+				difference = 0;
 			}
 
-			// basic math variables
+			// Time values
 			var _second = 1000,
 				_minute = _second * 60,
 				_hour = _minute * 60,
 				_day = _hour * 24;
 
-			// calculate dates
+			// Calculate elapsed time
 			var days = Math.floor(difference / _day),
 				hours = Math.floor((difference % _day) / _hour),
 				minutes = Math.floor((difference % _hour) / _minute),
 				seconds = Math.floor((difference % _minute) / _second);
 
-			// based on the date change the refrence wording
+			// Singular / plural text
 			var text_days = (days === 1) ? settings.day : settings.days,
 				text_hours = (hours === 1) ? settings.hour : settings.hours,
 				text_minutes = (minutes === 1) ? settings.minute : settings.minutes,
 				text_seconds = (seconds === 1) ? settings.second : settings.seconds;
 
-				// fix dates so that it will show two digets
-				days = (String(days).length >= 2) ? days : '0' + days;
-				hours = (String(hours).length >= 2) ? hours : '0' + hours;
-				minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
-				seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+			// Add leading zero
+			days = (String(days).length >= 2) ? days : '0' + days;
+			hours = (String(hours).length >= 2) ? hours : '0' + hours;
+			minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
+			seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
 
-			// set to DOM
+			// Update DOM
 			container.find('.days').text(days);
 			container.find('.hours').text(hours);
 			container.find('.minutes').text(minutes);
@@ -103,10 +94,14 @@
 			container.find('.hours_text').text(text_hours);
 			container.find('.minutes_text').text(text_minutes);
 			container.find('.seconds_text').text(text_seconds);
+
+			// Optional callback
+			if (callback && typeof callback === 'function') callback();
 		}
 
-		// start
-		var interval = setInterval(countdown, 1000);
+		// Start counting up
+		countup();
+		var interval = setInterval(countup, 1000);
 	};
 
 })(jQuery);
